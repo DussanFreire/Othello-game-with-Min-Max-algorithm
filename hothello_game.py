15c7ae5d8cf11e3226118a5ab758dae456bf9587
ASDIO1 <diego.an.sanchez.a@gmail.com>
2021-04-28 17:13:45 -0400

    Game board printing

--- hothello_game.py
+++ hothello_game.py
@@ -1,4 +1,5 @@
 import numpy as np
+import os
 
 class Settings:
     face_up = 'O'
@@ -11,24 +12,42 @@ class HeuristicFunction:
 
 class Player:
     def __init__(self, name, settings):
-        name = name
-        settings = settings
+        self.name = name
+        self.settings = settings
 
 class Token:
     def __init__(self, face_side):
-        face_side = face_side
+        self.face_side = face_side
 
 
 class Cell(object):
-    pass
-    #token
-    #def __init__(self, token):
-    #    token = token
+    def __init__(self, token="_"):
+        self.token = token
 
 class Board:
     def __init__(self, cells, size=8):
-        size = size
-        cells = cells
+        self.size = size
+        self.cells = cells
+    
+    def draw_board(self, fmt="g"):
+        white = "\033[1;37m"
+        color = "\033[0;33m"
+        numbers = range(1,9)
+        letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        i = 0
+        print(color,"\n")
+        
+        for n in numbers:
+            print(n, end="     ")
+        print("\n")
+
+        for row in cells:
+            for cell in row:
+                print(cell.token, end="     ")
+            print("",numbers[i] ,"\n")
+            i = i + 1
+        print(white, "\n")
+
 
 if __name__ == "__main__":
     settings = Settings()
@@ -46,3 +65,14 @@ if __name__ == "__main__":
     
     settings.heuristic = HeuristicFunction().heuristic1
     board = Board(cells)
+    
+    # os.system("cls")   NECESARIO.  Si los coclores no funcionan hay que hacer un cls
+    board.draw_board()
+
+
+""" Colors:
+        white = "\033[1;37m"
+        green = "\033[0;32m"
+        yellow = "\033[1;30m"
+        brown = "\033[0;33m"
+        """
